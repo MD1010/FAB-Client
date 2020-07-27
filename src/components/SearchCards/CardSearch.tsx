b0b921064b1416b55e729450d3a3434f5f0c07a8
Michael <michaelkatom10@gmail.com>
2020-07-27 15:22:30 +0300

    auto complete works - displaying id instead of name

--- src/components/SearchCards/CardSearch.tsx
+++ src/components/SearchCards/CardSearch.tsx
@@ -1,43 +1,82 @@
 import { AutoComplete } from 'antd';
 import axios from 'axios';
-import { debounce } from 'lodash';
+import { debounce, find } from 'lodash';
 import React, { FC, useState } from 'react';
 import { CARDS_ENDPOINT } from 'src/consts/endpoints';
 import { UTCard } from 'src/interfaces/UTCard';
 import { Container } from 'src/styles/common/Container';
 
 const CardSearch: FC = () => {
+  console.log(1);
   const [options, setOptions] = useState<UTCard[]>([]);
-  // const [searchedText, setSearchedText] = useState('');
-
-  const onSelect = (data: any) => {
-    console.log('onSelect', data);
+  // const [selectedValue, setSelectedValue] = useState<string | undefined>('');
+  const [textInput, setText] = useState<string | undefined>('');
+  const { Option } = AutoComplete;
+  const onSelect = (id: string) => {
+    let selected = find(options, (card: UTCard) => card.id === +id)?.name;
+    setText(selected);
+    // setSelectedValue(selected);
+    setOptions([]);
   };
   const searchCard = debounce(async (playerName: string) => {
     if (playerName.length > 2) {
       try {
-        const { data } = await axios.get(`${CARDS_ENDPOINT}/${playerName}`);
+        const { data } = await axios.get(`${CARDS_ENDPOINT}`, { params: { term: playerName } });
         setOptions(data);
       } catch (err) {
         throw err;
       }
     }
-  }, 500);
-  const { Option } = AutoComplete;
+  }, 400);
+
   return (
     <Container>
       <AutoComplete
+        open={options.length > 0}
         autoFocus
+        filterOption={false}
         style={{ width: '50vw' }}
-        onSelect={onSelect}
+        onSelect={(selected: string) => onSelect(selected)}
         onChange={(playerName) => searchCard(playerName)}
-        {...options.map((card: UTCard) => (
-          <Option key={card.id} value={card.name}>
-            <div>asdasd</div>
+      >
+        {options.map((card: UTCard) => (
+          <Option key={card.id} value={card.id.toString()}>
+            <div>{card.name}</div>
+            <div>{card.rating}</div>
+            <div>{card.id}</div>
           </Option>
         ))}
-      />
+      </AutoComplete>
     </Container>
   );
+  // const [result, setResult] = useState([]);
+
+  // const handleSearch = (value) => {
+  //   let res: any = [];
+
+  //   if (!value || value.indexOf('@') >= 0) {
+  //     res = [];
+  //   } else {
+  //     res = [{ name: 'gmail.com' }, { name: '163.com' }, { name: 'qq.com' }].map((domain) => `${value}@${domain.name}`);
+  //   }
+
+  //   setResult(res);
+  // };
+
+  // return (
+  //   <AutoComplete
+  //     style={{
+  //       width: 200,
+  //     }}
+  //     onSearch={handleSearch}
+  //     placeholder='input here'
+  //   >
+  //     {...result.map((email) => (
+  //       <Option key={email} value={email}>
+  //         {email}
+  //       </Option>
+  //     ))}
+  //   </AutoComplete>
+  // );
 };
 export default CardSearch;
---
+++ src/consts/ServerIP.ts
@@ -0,0 +1,1 @@
+export const SERVER_IP = 'http://192.168.0.100:5000';
